3524b03418fbd0322f9b0a2e1cd9ef9f4a385e6f
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2022-02-14 13:00:01 +0100

    strategy #1

--- code.js
+++ code.js
@@ -1,9 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // Initial clean up. DO NOT REMOVE.
-  initialCleanup();
+  initialCleanup()
 
-  // Hey! Pssst! In here ...
-});
+  document.getElementById('btn-add-line').addEventListener(
+    'click', addLine)
+  document.getElementById('btn-del-line').addEventListener(
+    'click', delLine)
+
+  for (const div of document.querySelectorAll('#grid>div')) {
+    initDiv(div)
+  }
+  updateTotals()
+})
+
+function initDiv(div) {
+  div.addEventListener('click',
+    () => { setRandomColor(div); updateTotals() })
+  div.addEventListener('mouseenter',
+    () => { setHoverColor(div); updateTotals() })
+}
 
 /**
  * Cleans up the document so that the exercise is easier.
@@ -12,13 +27,63 @@ document.addEventListener("DOMContentLoaded", () => {
  * to clean them up beforehand.
  */
 function initialCleanup() {
-  const nodesToRemove = [];
+  const nodesToRemove = []
   document.getElementById("grid").childNodes.forEach((node, key) => {
     if (node.nodeType !== Node.ELEMENT_NODE) {
-      nodesToRemove.push(node);
+      nodesToRemove.push(node)
     }
-  });
+  })
   for (const node of nodesToRemove) {
-    node.remove();
+    node.remove()
   }
+}
+
+function addLine() {
+  const grid = document.getElementById('grid')
+  for (let i = 0; i < 10; i++) {
+    let div = document.createElement('div')
+    grid.append(div)
+    initDiv(div)
+  }
+  updateTotals()
+}
+
+function delLine() {
+  const grid = document.getElementById("grid")
+  for (let i = 0; i < 10; i++) {
+    grid.removeChild(grid.lastChild)
+  }
+  updateTotals()
+}
+
+function setRandomColor(element) {
+  element.style.backgroundColor = randomColor()
+  element.classList.remove('hovered')
+}
+
+function setHoverColor(element) {
+  //  console.log('hover')
+  element.style.backgroundColor = ''
+  element.classList.add('hovered')
+}
+
+function randomColor() {
+  // a random (float) number between 0 and 2**24-1
+  const F = (1 << 24 - 1) * Math.random()
+  // floor it to an int
+  const N = Math.floor(F)
+  // convert to hexa and build a color
+  return `#${N.toString(16)}`
+}
+
+function updateTotals() {
+  //  console.log('totals')
+  const total = document.querySelectorAll("#grid>div").length
+  const clicked = document.querySelectorAll('#grid div[style*="background-color"]').length
+  const blue = document.querySelectorAll('#grid div.hovered').length
+  const original = total - clicked - blue
+  document.querySelector("#totals #original").innerHTML = original.toString()
+  document.querySelector("#totals #clicked").innerHTML = clicked.toString()
+  document.querySelector("#totals #blue").innerHTML = blue.toString()
+  document.querySelector("#totals #total").innerHTML = total.toString()
 }
